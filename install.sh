36f546f38ce57a2016cbca59b36bcf0776911687
Yasin <140549630+Symbolexe@users.noreply.github.com>
2024-05-18 13:15:49 +0330

    PhantomProbe Files

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo apt update && sudo apt upgrade && sudo apt install python3-pip ruby perl go
 pip3 install -r emails/theHarvester/requirements/base.txt
 pip3 install requests_file requests jsbeautifier lxml argparse dnspython aiohttp aiomultiprocess censys aiodns shodan aiosqlite playwright google arjun
 gem install wpscan
-echo " Now Type : python3 main.py To Run PhantomProbe Framework
+echo "Now Type : python3 main.py To Run PhantomProbe Framework"
